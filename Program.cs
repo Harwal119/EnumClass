7f85e3b4e21df9c26c9d0b2f779b321153be353c
Harwal119 <awwal6823@gmail.com>
2023-06-05 22:55:44 +0100

    Coder

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!");
+        Console.WriteLine((int)DaysOfTheWeek.friday);
+        // int num = 3;
+        // DaysOfTheWeek anotherday = (DaysOfTheWeek)num;
+        // Console.Write(anotherday);
+        // string[] names = Enum.GetNames(typeof(DaysOfTheWeek));
+        // Console.WriteLine(names);
+        // int[] nums (int[])Enum.GetValues(typeof(DaysOfTheWeek));
+        // foreach(var nums in nums)
+        // {
+        //     Console.WriteLine(nums);
+        // }
+
+//         static void PrintDayMessage(DaysOfTheWeek day)
+// {
+//     Console.WriteLine($"Today is {day}")
+// }
+//  PrintDayMessage(DaysOfTheWeek.friday);
+
+}
+}
+
+enum DaysOfTheWeek
+
+{
+    monday = 1,
+    tuesday,
+    wednesday,
+    thursday,
+    friday,
+    saturday,
+    sunday
+}
+
